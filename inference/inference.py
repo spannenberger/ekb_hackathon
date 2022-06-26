203c84187cf8888015c7c7226ac61dfece88f405
Egor <e.morozov>
2022-06-26 11:59:20 +0500

    fixes inference

--- inference/inference.py
+++ inference/inference.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 import argparse
 import os
 import pandas as pd
+import numpy as np
+import base64
 
 from utils import draw_contours
 
@@ -27,15 +29,23 @@ URL = 'http://10.10.67.145:5010/api/ekb_service'
 content_type = 'image/jpeg'
 headers = {'content-type': content_type}
 
+
 def request2back(img_path: str) -> Dict:
-    image_array = cv2.imread(img_path)
-    _, img_encoded = cv2.imencode('.jpg', image_array)
+    with open(img_path, 'rb') as ph:
+        data = ph.read()
+    image_array = np.asarray(bytearray(data), dtype="uint8")
+    
+    img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    _, img_encoded = cv2.imencode('.jpg', img)
+
     data = img_encoded.tostring()
 
     response = requests.post(URL, data=data, headers=headers)
     metadata = response.json()['image']
     
-    return metadata, image_array
+    return metadata, img
 
 
 def main():
@@ -57,9 +67,9 @@ def main():
         img_name = os.path.basename(img_path)
         cv2.imwrite(f"{recognitions_dir}/{img_name}", img)
 
-        dataframe_result = dataframe_result.append({'img_name':img_name, 'all_teeth_count':all_teeth_count,\
-                                                     'teeth_count':teeth_count, 'caries_count':caries_count,\
-                                                     'caries_bool':caries_bool}, ignore_index=True)
+        dataframe_result = dataframe_result.append({'filename':img_name,\
+                    # 'all_teeth_count':all_teeth_count,'teeth_count':teeth_count, 'caries_count':caries_count,\
+                                                     'Caries':caries_bool}, ignore_index=True)
 
     elif path2images:
         print('Идет распознавание изображений из указанной директории')
@@ -75,9 +85,9 @@ def main():
 
             cv2.imwrite(f"{recognitions_dir}/{image}", img)
 
-            dataframe_result = dataframe_result.append({'img_name':image, 'all_teeth_count':all_teeth_count,\
-                                                    'teeth_count':teeth_count, 'caries_count':caries_count,\
-                                                    'caries_bool':caries_bool}, ignore_index=True)
+            dataframe_result = dataframe_result.append({'filename':image,\
+                #  'all_teeth_count':all_teeth_count,'teeth_count':teeth_count, 'caries_count':caries_count,\
+                                                    'Caries':caries_bool}, ignore_index=True)
 
         
 
